481dc1dadcbc6993f996aff9e6b6038fc22fa2e6
crowdani@uk.ibm.com <crowdani@uk.ibm.com@Daniels-MacBook-Pro-4.local>
2020-10-08 17:16:20 +0100

    update  commit

--- routes/slackamc.js
+++ routes/slackamc.js
@@ -8,17 +8,19 @@ var tokenMaker = require('basic-auth-token');
 router.post('/', function (req, res) {
 	console.log(req.body.payload.properties)
 	 console.log(req.body.payload)
+	 console.log('Hello ' + process.env.slackurl)
 
+	 
 	var data = JSON.stringify({"text":"Hello, World!"});
 	var config = {
   		method: 'post',
-  		url: 'https://hooks.slack.com/services/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',
+  		url: process.env.slackurl,
   		headers: { 
     		'Content-Type': 'application/json'
   		},
   		data : data
 	};
-
+console.log('Hello ' + process.env.NAME)
 
 	axios(config).then(function (response) {
 			res.send(req.body);
