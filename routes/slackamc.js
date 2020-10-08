6452c67a429f18d48a63e266d826865431af7750
crowdani@uk.ibm.com <crowdani@uk.ibm.com@Daniels-MacBook-Pro-4.local>
2020-10-08 16:47:23 +0100

    update  commit

--- routes/slackamc.js
+++ routes/slackamc.js
@@ -9,9 +9,27 @@ router.post('/', function (req, res) {
 	console.log(req.body.payload.properties)
 	 console.log(req.body.payload)
 
-	 res.send(req.body);
+	var data = JSON.stringify({"text":"Hello, World!"});
+	var config = {
+  		method: 'post',
+  		url: 'https://hooks.slack.com/services/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',
+  		headers: { 
+    		'Content-Type': 'application/json'
+  		},
+  		data : data
+	};
+
+
+	axios(config).then(function (response) {
+			res.send(req.body);
+	}).catch(function (error) {
+    console.log("Error:" + error);
+    res.send(error);
+  });
 
 })
+
+
 
 module.exports = router
 
